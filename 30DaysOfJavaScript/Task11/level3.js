31e1fe8efae10b21a7f0b87ef253c53bf430b79d
Alenn Wright <bowserko123@gmail.com>
2022-06-21 20:18:08 -0700

    Added Task 11
    Finished level 3

--- 30DaysOfJavaScript/Task11/level3.js
+++ 30DaysOfJavaScript/Task11/level3.js
@@ -1,7 +1,14 @@
 // Destructure the countries object print name, capital, population and languages of all countries
+function namecaplang(data){
+  for(const country of data){
+    console.log(`${country.name},${country.capital},${country.population}`)
+  }
+}
+namecaplang(countries_data)
 
 // A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
 const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+const [name, skills, [,,jsScore, reactScore]] = student
 console.log(name, skills, jsScore, reactScore)
 // David (4) ["HTM", "CSS", "JS", "React"] 90 95
 
@@ -9,22 +16,17 @@ console.log(name, skills, jsScore, reactScore)
 const students = [
     ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
     ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
-  ]
-
-console.log(convertArrayToObject(students))
-[
-  {
-    name: 'David',
-    skills: ['HTM','CSS','JS','React'],
-    scores: [98,85,90,95]
-  },
-  {
-    name: 'John',
-    skills: ['HTM','CSS','JS','React'],
-    scores: [85, 80,85,80]
-  }
 ]
 
+function convertArrayToObject(List){
+  objectList = []
+  for(const student of List){
+    objectList.push({name:student[0],skills:student[1],scores:student[2]})
+  }
+  return objectList
+}
+
+console.log(convertArrayToObject(students))
 
 // Copy the student object to newStudent without mutating the original object. In the new object add the following ?
 // Add Bootstrap with level 8 to the front end skill sets
@@ -48,9 +50,13 @@ const student1 = {
       dataBase:[
         { skill: 'MongoDB', level: 7.5 },
       ],
-      dataScience:['Python', 'R', 'D3.js']
-    }
+    dataScience:['Python', 'R', 'D3.js']
   }
+}
 
-
-
+const newStudent = student1
+newStudent.skills.frontEnd.push({skill:'Bootstrap',level:8})
+newStudent.skills.backEnd.push({skill:'Express',level:9})
+newStudent.skills.dataBase.push({skill:'SQL',level:8})
+newStudent.skills.dataScience.push('SQL')
+console.log(newStudent)
