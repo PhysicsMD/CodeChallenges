07ce6f74f3b32085341fd47e9a9b39b044f29139
Alenn Wright <bowserko123@gmail.com>
2022-07-02 15:50:51 -0700

    Added Task 14
    Error throwing

---
+++ 30DaysOfJavaScript/Task14/practice.js
@@ -0,0 +1,16 @@
+//you can throw an error for ex.
+throw 'Error big virus'
+
+//in a try catch you can utilize different trypes of exceptions
+//Syntax
+let square = 2 x 2
+console.log(square)
+
+//Type
+let num = 10
+console.log(num.toLowerCase())
+
+//Reference 
+let firstName = 'Asabeneh'
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
